71b01aa17443dbab13a9ffcb16b7f5ac477650cf
cris <cris@mail.com>
2022-04-16 18:12:49 -0400

    conf

--- staticproject/wsgi.py
+++ staticproject/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+
+project_home = '/home/crissirona/coni/coni'
+if project_home not in sys.path:
+    sys.path.insert(0,project_home)
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'staticproject.settings')
+os.environ['DJANGO_SETTINGS_MODULE']= 'staticproject.settings'
 
 application = get_wsgi_application()
